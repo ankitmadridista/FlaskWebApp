1f7ad13667ea59cb41a9635ac03cef24312a54a4
asuravk <Ankit.Suravkar@morningstar.com>
2022-08-09 12:40:24 +0530

    fist commit

---
+++ website/views.py
@@ -0,0 +1,38 @@
+from flask import Blueprint, render_template, request, flash, jsonify, url_for
+from flask_login import login_required, current_user
+from werkzeug.utils import redirect
+
+from . import db
+from .models import Note
+import json
+
+views = Blueprint('views', __name__)
+
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
+def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+        if len(note) < 1:
+            flash('Note is empty', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added successfully', category='success')
+            return redirect(url_for('views.home'))
+    return render_template("home.html", user=current_user);
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
